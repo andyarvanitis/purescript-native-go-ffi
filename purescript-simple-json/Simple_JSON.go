4e3499035f0cb694f701d20f8005b9b865354034
Alvaro Vilanova Vidal <alvivi@gmail.com>
2019-09-03 20:40:37 +0200

    Simple.JSON._parseJSON added

---
+++ purescript-simple-json/Simple_JSON.go
@@ -0,0 +1,21 @@
+package purescript_global
+
+import (
+	"encoding/json"
+
+	. "github.com/purescript-native/go-runtime"
+)
+
+func init() {
+	exports := Foreign("Simple.JSON")
+
+	exports["_parseJSON"] = func(text_ Any) Any {
+		text := text_.(string)
+		var result Any
+		err := json.Unmarshal([]byte(text), &result)
+		if err != nil {
+			panic(err)
+		}
+		return result
+	}
+}

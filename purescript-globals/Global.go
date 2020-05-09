6dd6d85daa2db67049b009bb71af7407f13a7e12
Emad Shaaban <emad.shaaban92@gmail.com>
2020-05-10 00:26:15 +0200

    Implement Global readFloat and isNaN (#13)

--- purescript-globals/Global.go
+++ purescript-globals/Global.go
@@ -3,11 +3,25 @@ package purescript_global
 import (
 	. "github.com/purescript-native/go-runtime"
 	"math"
+	"strconv"
 )
 
 func init() {
 	exports := Foreign("Global")
 
 	exports["infinity"] = math.Inf(1)
+
+	exports["readFloat"] = func(x_ Any) Any {
+		x, _ := x_.(string)
+		f, err := strconv.ParseFloat(x, 64)
+		if err == nil {
+			return f
+		}
+		return math.NaN()
+	}
+	exports["isNaN"] = func(x_ Any) Any {
+		x, _ := x_.(float64)
+		return math.IsNaN(x)
+	}
 
 }
